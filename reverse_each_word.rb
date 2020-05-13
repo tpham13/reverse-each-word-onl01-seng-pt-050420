e5da41806ba37cd22322c7305f7fb3b170ec66b4
Thanh Pham <tpham0123@gmail.com>
2020-05-13 03:36:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,9 @@
-array = ["Hello there, and how are you?"]
-def reverse_each_word(string)
-  array.each do |string|
+def reverse_each_word(string) 
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
   
